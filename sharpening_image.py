9cf84889d9defc7d8457e9251d7bea36a3728573
shushant kumar <104075647+everythingcanbedone@users.noreply.github.com>
2022-07-26 15:37:49 +0530

    Update sharpening_image.py

--- sharpening_image.py
+++ sharpening_image.py
@@ -2,6 +2,8 @@ import
 
 img=cv2.imread("oipp.jpg")
 cv2.imshow("image",img)
+# can change the value and play with different numbers
+# for max sharpness set [1,1] to camparable max value
 k_sharpene=np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
 sharpened=cv2.filter2D(img,-1,k_sharpene)
 cv2.imshow("shar",sharpened)
